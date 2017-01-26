40ef3d57bd681eb09c0c68d9ec39fd337c71a521
coriumalpha <toroidalmirror@gmail.com>
2017-01-26 15:51:24 +0100

    Aclaración-comentarios PHP

--- Proyectos/NFC/NFC_PHP_Tester/api.php
+++ Proyectos/NFC/NFC_PHP_Tester/api.php
@@ -7,10 +7,19 @@ $identificadorRecibido = $_REQUEST['data'];
 
 if($identificadorRecibido == $tarjetaValida)
 {
+	/*
+	*	El código Arduino proporcionado (httpClient) como ejemplo
+	*	responde a esta palabra clave llamando a la función success.
+	*/
+
 	echo "ACCESS_GRANTED";
 }
 else
 {
+	/*
+	*	Devuelve el dato que ha recibido, útil para comprobar integridad.
+	*/
+	
 	echo "ACCESS DENIED FOR ID ".$identificadorRecibido;
 }
 
