222272c0f6cd245934598d85e5da9bd74a7edeb6
coriumalpha <toroidalmirror@gmail.com>
2017-01-26 18:08:08 +0100

    Syntax error :(

--- Proyectos/NFC/NFC_PHP_Tester/api.php
+++ Proyectos/NFC/NFC_PHP_Tester/api.php
@@ -1,25 +1,16 @@
 <?php
 
 //Identificador de tarjeta válida
-$tarjetaValida = "5bb282cb"
+$tarjetaValida = "5bb282cb";
 
 $identificadorRecibido = $_REQUEST['data'];
 
 if($identificadorRecibido == $tarjetaValida)
 {
-	/*
-	*	El código Arduino proporcionado (httpClient) como ejemplo
-	*	responde a esta palabra clave llamando a la función success.
-	*/
-
 	echo "ACCESS_GRANTED";
 }
 else
 {
-	/*
-	*	Devuelve el dato que ha recibido, útil para comprobar integridad.
-	*/
-	
 	echo "ACCESS DENIED FOR ID ".$identificadorRecibido;
 }
 
